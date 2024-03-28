a97ca504edcc268702146507de9beb7763488ed8
Facundo Ríos <Facundorios2005@gmail.com>
2024-03-28 16:02:26 -0300

    Finished work

--- src/App.jsx
+++ src/App.jsx
@@ -14,28 +14,22 @@ export function App() {
         const { fact } = data;
         setFact(fact);
 
-        const threeFirstWords = fact.split(" ", 3).join(" ");
+        const threeFirstWords = fact.split(" ", 3).join(" ")
         console.log(threeFirstWords);
 
-    fetch(`https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=red&json=true`)
-      .then(res => res.json())
-      .then(response => {
-        console.log(response)
-        const { url } = data
-        setimageURL(url)
-      });
+        setimageURL(`https://cataas.com/cat/says/${encodeURIComponent(threeFirstWords)}?fontSize=50&fontColor=red`)
   });
 }, []);
 
   return (
     <main>
-      <h1>App de gatitoides</h1>
+      <h1>App de gatitoides</h1>  
 
       {fact && <p>{fact}</p>}
 
       { 
         <img
-          src={`${CAT_PREFIX_URL}${imageURL}`}
+          src={`${imageURL}`}
           alt={`imagen extraida utilizando las 3 primeras palabras de: ${fact}`}
         />
       }
